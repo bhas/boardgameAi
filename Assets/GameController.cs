95dae2f5aaa9922109902830096934f6a125f55f
sm1rt19 <sm1rt19@lortemail.com>
2020-03-28 18:22:04 +0100

    Implemented tile, cross and circle

---
+++ Assets/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Tile tile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tile.SetState(TileState.Circle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public TileState state;
+    public GameObject cross;
+    public GameObject circle;
+
+    public void SetState(TileState state)
+    {
+        this.state = state;
+
+        switch (state)
+        {
+            case TileState.Empty:
+                cross.SetActive(false);
+                circle.SetActive(false);
+                break;
+            case TileState.Cross:
+                cross.SetActive(true);
+                circle.SetActive(false);
+                break;
+            case TileState.Circle:
+                cross.SetActive(false);
+                circle.SetActive(true);
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+    }
+}
+
+public enum TileState
+{
+    Empty = 0,
+    Cross = 1,
+    Circle = 2
+}
