22db726fed2a7163a5d1caf8078dcb7b892d2f67
sm1rt19 <sm1rt19@lortemail.com>
2020-04-10 20:52:16 +0200

    Added mouse click

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -16,41 +16,22 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetMouseButtonDown(0))
         {
-            controller.MakeMove(tileState, 0);
+            DetectMouseClick();
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+    }
+
+    public void DetectMouseClick()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            controller.MakeMove(tileState, 1);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            controller.MakeMove(tileState, 2);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            controller.MakeMove(tileState, 3);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            controller.MakeMove(tileState, 4);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            controller.MakeMove(tileState, 5);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha7))
-        {
-            controller.MakeMove(tileState, 6);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha8))
-        {
-            controller.MakeMove(tileState, 7);
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha9))
-        {
-            controller.MakeMove(tileState, 8);
+            if (hit.collider.CompareTag("Tile"))
+            {
+                var tile = hit.collider.GetComponent<Tile>();
+                controller.MakeMove(tileState, tile.id);
+            }
         }
     }
 }
